1404f5410997bc47cc0ddc8b67490ceb451822bb
Tom Bird <thomasbird1984@gmail.com>
2020-10-31 16:01:22 -0400

    Removing middleware

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'apiToken' => \App\Http\Middleware\UserToken::class,
+        // 'apiToken' => \App\Http\Middleware\UserToken::class,
     ];
 }
