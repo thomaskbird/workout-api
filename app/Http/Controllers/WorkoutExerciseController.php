ea9411d70e8728a063ec4cf2cccdec2f9725e597
Tom Bird <thomasbird1984@gmail.com>
2020-11-02 20:26:30 -0500

    Adding use statement for validator

--- app/Http/Controllers/WorkoutExerciseController.php
+++ app/Http/Controllers/WorkoutExerciseController.php
@@ -1,8 +1,8 @@
 <?php namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\WorkoutExercise;
 
